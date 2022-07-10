6c49b8effc58b35455efb8c0b1edd1828f164163
nirvana <nrvnanrmnd@gmail.com>
2022-07-10 20:46:47 +0430

    init nirvana

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './components/product/products/products.component';
+
+const routes: Routes = [{ path: 'products', component: ProductsComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
